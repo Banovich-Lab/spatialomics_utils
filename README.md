7ea8195801abcc5867a31f2e9a2a4151b5d26628
sondvo <duyson1999@gmail.com>
2026-06-27 11:02:25 -0700

    load ome tif

--- README.md
+++ README.md
@@ -10,10 +10,14 @@ Functions for spatial omics data analysis
 
 
 ### 2. Mask dilation
-- Broaden the "mask" to the black pixels within the defined radius
+- Image dilation: Broaden the "mask" to the black pixels, within the defined radius
+- Coordinates dilation: Dilate "Unlabeled" point to its nearest "Labeled" point, within the defined radius
 
 ![segment and dilate](img/demo.gif)
 
 ### 3. Convert Anndata <-> Rds
 - Mitigate dependency on other pre-build packages
 - Break anndata/rds object into small components (.mtx, .csv), then build up rds/anndata
+
+### 4. Reading Ome.Tiff
+- Lazyly crop Ome Tiff image without loading huge image to the RAM
---
+++ spatialomics_utils/loading_ome_tif.py
@@ -0,0 +1,53 @@
+import zarr
+import tifffile         
+import numpy as np
+
+
+def lazy_load(
+        tif_path: str, 
+        level: int, 
+        ystart: int, 
+        yend: int, 
+        xstart: int, 
+        xend: int
+    ) -> np.ndarray:
+    """
+    Lazily load an OME-TIFF file using zarr.
+
+    Parameters
+    ----------
+    tif_path : str
+        Path to the OME-TIFF file.
+    level : int
+        The level of the pyramid to load.
+    ystart : int
+        The starting y-coordinate.
+    yend : int
+        The ending y-coordinate.
+    xstart : int
+        The starting x-coordinate.
+    xend : int
+        The ending x-coordinate.
+
+    Returns
+    -------
+    np.ndarray
+        An array representing the OME-TIFF data.
+    """
+    try:
+        store = tifffile.imread(
+            tif_path,
+            return_as='zarr'
+        )
+        lazy_img = zarr.open(store, mode='r')
+
+    except Exception as e:
+        print(f"Error loading OME-TIFF file: {e}")
+        print("Suggested version: tifffile==2026.6.1 zarr==3.2.1")
+        return None
+
+    # Check the groups
+    print("Ome TIF Group", lazy_img.tree()) 
+
+    square_crop = lazy_img[level][..., ystart:yend, xstart:xend]
+    return square_crop
